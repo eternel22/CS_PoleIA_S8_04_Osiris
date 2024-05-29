d2ad2b13719ccdf3c54e66de53c86c62848ca55a
Jaillet Loic <loic.jaillet@student-cs.fr>
2024-05-29 14:54:48 +0000

    Convert files xlsx -> csv

--- convert_xlsx_csv.py
+++ convert_xlsx_csv.py
@@ -1,44 +1,7 @@
 import pandas as pd
 import pandas as pd
 import os
-"""
-path = 'DATA/Probes/'
-path1 = path + 'Sonde Canon 2.xlsx'
 
-df = pd.read_excel(path1)
-# Delete the first line of df
-
-# Convert to CSV file
-csv_path = 'DATA/Probes/Sonde Canon 2.csv'
-df.to_csv(csv_path, index=False, sep=';')
-
-df_csv = pd.read_csv(csv_path, skiprows=1)
-
-first_line = pd.read_csv(csv_path).iloc[0]
-
-df_csv.to_csv(csv_path, index=False)
-df_csv = pd.read_csv(csv_path)
-
-if 'Température de l\'air [°C]' in first_line:
-    df_csv = df_csv.iloc[:, [0] + list(range(4, len(df_csv.columns)))]
-if 'Température sèche [°C]' in first_line:
-    df_csv = df_csv.iloc[:, [0] + list(range(4, len(df_csv.columns)))]
-
-header = ['Date/heure','Précipitations [mm]','EAG Humidité du sol 1 [%]',
-          'EAG Humidité du sol 2 [%]','EAG Humidité du sol 3 [%]','EAG Humidité du sol 4 [%]','EAG Humidité du sol 5 [%]',
-          'EAG Humidité du sol 6 [%]','Température du sol MOY 1 [°C]','Température du sol MAX 1 [°C]','Température du sol MIN 1 [°C]',
-          'Température du sol MOY 2 [°C]','Température du sol MAX 2 [°C]','Température du sol MIN 2 [°C]','Température du sol MOY 3 [°C]',
-          'Température du sol MAX 3 [°C]','Température du sol MIN 3 [°C]','Température du sol MOY 4 [°C]','Température du sol MAX 4 [°C]',
-          'Température du sol MIN 4 [°C]','Température du sol MOY 5 [°C]','Température du sol MAX 5 [°C]','Température du sol MIN 5 [°C]',
-          'Température du sol MOY 6 [°C]','Température du sol MAX 6 [°C]','Température du sol MIN 6 [°C]',
-          'Panneau solaire [mV]','Batterie [mV]']
-
-header_to_str = ';'.join(header)
-df_csv.columns = [header_to_str]
-
-# Save the modified CSV file
-df_csv.to_csv(csv_path, index=False)
-"""
 class Converter:
     def __init__(self, path):
         self.path = path
@@ -50,21 +13,21 @@ class Converter:
             
         df = pd.read_excel(path)
         df = df.iloc[1:]  # Delete the first line of df
-
+        
         csv_path = path.replace('.xlsx', '.csv')
         df.to_csv(csv_path, index=False, sep=';')
         
-        first_line = pd.read_csv(csv_path).iloc[0]
-        df_csv = pd.read_csv(csv_path, skiprows=1)
+        first_line = pd.read_csv(csv_path,sep=';').columns
+        
+        df_csv = pd.read_csv(csv_path, skiprows=0, sep=';')
 
-        df_csv.to_csv(csv_path, index=False)
-        df_csv = pd.read_csv(csv_path)
-
+        df_csv.to_csv(csv_path, index=False, sep = ';')
+        df_csv = pd.read_csv(csv_path, sep=';')
         if 'Température de l\'air [°C]' in first_line:
             df_csv = df_csv.iloc[:, [0] + list(range(4, len(df_csv.columns)))]
         if 'Température sèche [°C]' in first_line:
             df_csv = df_csv.iloc[:, [0] + list(range(4, len(df_csv.columns)))]
-
+            
         header = ['Date/heure', 'Précipitations [mm]', 'EAG Humidité du sol 1 [%]',
                   'EAG Humidité du sol 2 [%]', 'EAG Humidité du sol 3 [%]', 'EAG Humidité du sol 4 [%]',
                   'EAG Humidité du sol 5 [%]', 'EAG Humidité du sol 6 [%]', 'Température du sol MOY 1 [°C]',
@@ -77,9 +40,9 @@ class Converter:
                   'Batterie [mV]']
 
         header_to_str = ';'.join(header)
-        df_csv.columns = [header_to_str]
+        df_csv.columns = header
 
-        df_csv.to_csv(csv_path, index=False)
+        df_csv.to_csv(csv_path, index=False, sep=';')
 
     def folder_convert(self):
         for file in os.listdir(self.path):
